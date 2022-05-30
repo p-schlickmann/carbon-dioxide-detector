43ac408dcb275ebeb7180012be71c7adff15cfdd
mendespedro <windowsxpedro@gmail.com>
2022-05-30 16:20:14 -0300

    firmware code updated

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import requests
 from fastapi import FastAPI
 
--- firmware/Firmware.ino
+++ firmware/Firmware.ino
@@ -1,15 +1,16 @@
-// Adicionar bibliotecas
-//#include "MQ7.h"
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include "MQ7.h"
 
-// Definir rede
 #ifndef STASSID
-#define STASSID "Bruno";
-#define STAPSK  "12345678";
+#define STASSID "iPhone do pepa"
+#define STAPSK  "pedro2022"
 #endif
+
+const int ANALOG_SENSOR_PIN = A0;
+MQ7 mq(ANALOG_SENSOR_PIN, 5.0);  
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
@@ -18,34 +19,12 @@ ESP8266WebServer server(80);
 
 const int led = 13;
 
-// Definições iniciais
-//#define MQ7_PIN_AOUT	A0
-//
-//
-// Inicializando objetos necessários
-// MQ7 mq7(MQ7_PIN_AOUT);
-
 void handleRoot() {
-//   int mq7Val = mq7.read();
-//   mq7Val = map(mq7Val, 40, 120, 0, 255);
-//   Serial.print(F("raw: ")); Serial.println(mq7.read());
-//   Serial.print(F("val: ")); Serial.println(mq7Val);
-  server.send(200, "text/plain", "3000");
-}
-
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
+  digitalWrite(led, 1);
+  float ppm = mq.getPPM();
+  Serial.print("ppm CO: ");
+  Serial.println(ppm);
+  server.send(200, "text/plain", String(ppm));
   digitalWrite(led, 0);
 }
 
@@ -53,8 +32,6 @@ void setup(void) {
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
-  while (!Serial) ; // Esperar para a porta do Serial conectar
-  Serial.println("start");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -75,8 +52,6 @@ void setup(void) {
   }
 
   server.on("/", handleRoot);
-
-  server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
